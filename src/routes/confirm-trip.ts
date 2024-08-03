a0943c663620b2170aa1f355b029964c5719154d
Jean Oliveira <jcwilbertt@gmail.com>
2024-08-04 00:17:48 +0100

    confirm-trip-redirect

--- src/routes/confirm-trip.ts
+++ src/routes/confirm-trip.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod";
+import { prisma } from "../lib/prisma";
 
 export async function confirmTrip(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().get(
@@ -11,7 +12,28 @@ export async function confirmTrip(app: FastifyInstance) {
           tripId: z.string().uuid(),
         }),
       },
-    }, async (request) => {
+    }, async (request, reply) => {
+      const { tripId } = request.params
+
+      const trip = await prisma.trip.findUnique({
+        where: {
+          id: tripId,
+        }
+      })
+
+      if (!trip) {
+        throw new Error('Trip not found.')
+      }
+
+      if (trip.is_confirmed) {
+        // Deve retornar o endereço da porta do frontend
+        return reply.redirect(`http://localhost:5173/trips/${tripId}`)
+      }
+
+      await prisma.trip.update({
+        where: { id: tripId },
+        data: { is_confirmed: true },
+      })
       
       return { tripId: request.params.tripId };
     },
